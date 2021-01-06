660c31433a630ab19614ce52bed9237914d6b309
jykim <jykim@medicalstandard.com>
2021-01-06 17:50:34 +0900

    vue test 11

--- vue-todo/src/App.vue
+++ vue-todo/src/App.vue
@@ -1,7 +1,8 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App 222"/>
+    <!-- <HelloWorld msg="Welcome to Your Vue.js App 333" s1="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" :my-message="msg"/> -->
+    <hello-world msg="Welcome to Your Vue.js App 555" s1="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" :my-message="msg"/>
   </div>
 </template>
 
@@ -10,6 +11,12 @@ import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
+  data () {
+    return {
+      msg : '안녕하세요 !!'
+    }
+
+  },
   components: {
     HelloWorld
 
